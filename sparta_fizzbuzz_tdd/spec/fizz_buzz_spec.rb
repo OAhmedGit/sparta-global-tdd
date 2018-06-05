6ed22ff5b06291c937a973b08992d77bcf57637a
Osama Ahmed <oahmed@spartaglobal.com>
2018-06-05 22:47:41 +0100

    Refactoring the code

--- sparta_fizzbuzz_tdd/spec/fizz_buzz_spec.rb
+++ sparta_fizzbuzz_tdd/spec/fizz_buzz_spec.rb
@@ -6,7 +6,11 @@ describe FizzBuzzEngine do
     # and creates the array of numbers
     before(:all) do
         @fizz_buzz = FizzBuzzEngine.new
-        @fizz_buzz.create_array
+    end
+
+    # Checks if the object type is an array and between 1 and 100
+    it 'should be an array between 1 - 100' do
+        expect(@fizz_buzz.create_array).to eq((1..100).to_a)
     end
 
     # Checks if the number 3 is Fizz in the array
@@ -29,9 +33,9 @@ describe FizzBuzzEngine do
 
     # Checks if the array is empty, if false the test is passed 
     it 'should print the array elements' do
-        result = @fizz_buzz.output_array
-
+        result = @fizz_buzz.fizz_buzz_check
         expect(result.empty?).to eq(false)
+        @fizz_buzz.output_array
     end
 
 end
